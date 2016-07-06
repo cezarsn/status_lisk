d234096a51f2e7b1514ad917a661f50a069eca1a
Cezar Spatariu-Neagu <cezar.spatariu@gmail.com>
2016-07-06 10:31:49 +0200

    template changes for mobile

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #https://www.digitalocean.com/community/tutorials/how-to-structure-large-flask-applications
 
 from status_lisk import app
-app.run(debug = True, port=8083 )
+app.run(host="192.168.178.31", debug = True, port = 8083)
