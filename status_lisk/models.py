8e73da25ca17b46dd4fd6e9826993c288d5e71cb
= <=>
2016-07-20 09:29:28 +0200

    Implement Last forged block over API

--- status_lisk/models.py
+++ status_lisk/models.py
@@ -1,7 +1,6 @@
 import controllers
 
 
-
 class Server(object):
     def __init__(self, protocol, host, port, name, net):
         self.protocol = protocol
@@ -9,14 +8,30 @@ class Server(object):
         self.port = port
         self.name = name
         self.url = '{}://{}:{}/'.format(self.protocol, self.host, self.port)
-	self.net = net
-        self.status = self.get_block_heigh()
+        self.net = net
+        self.status = self.get_block_height()
+        self.last_forged = self.get_last_forged("")
+
+    def get_block_height(self):
+        try:
+            block_height = controllers.make_request(self.url, 'api/loader/status/sync', "")['height']
+            return block_height
+        except TypeError:
+            return False
+
+    def get_last_forged(self, public_key):
+        """
+        /api/blocks?generatorPublicKey=0348a623c41ed7742a7f35a5812476750e2ba41208e16a29b110e6fe11e514d4&limit=1&orderBy=height:desc
+
+       {"success":true,"blocks":[{"id":"9970641156066037716","version":0,"timestamp":4861570,"height":240604,"previousBlock":"28091283506320323","numberOfTransactions":0,"totalAmount":0,"totalFee":0,"reward":500000000,"payloadLength":0,"payloadHash":"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855","generatorPublicKey":"0348a623c41ed7742a7f35a5812476750e2ba41208e16a29b110e6fe11e514d4","generatorId":"16379340065696424247L","blockSignature":"5e2edfcf73f76d9b57386e87bcef0a18efc5e5fe96ee1aa38ca3f9e4c51f4268d930663b830b885d1712e16a45e76c7bd41cc950c409b0373dfe6b25ff245e00","confirmations":31,"totalForged":"500000000"}],"count":1934}
 
 
-    def get_block_heigh(self):
-	try:
-            block_heigh = controllers.make_request(self.url, 'api/loader/status/sync', "")['height']
-            return block_heigh
+        :return:
+        """
+        public_key = "0348a623c41ed7742a7f35a5812476750e2ba41208e16a29b110e6fe11e514d4"
+        try:
+            last_forged = controllers.make_request(self.url, '/api/blocks', {"generatorPublicKey": public_key, "orderBy": "height:desc", "limit": 1, })
+            return last_forged
         except TypeError:
             return False
 
@@ -33,4 +48,3 @@ class Delegate(object):
 
     def __str__(self):
         return 'Lisk Address:{} -Name:{} -Rank:{}'.format(self.lisk_address, self.name, self.rank)
-
