f6f8c42a69839548c87f815222eafc74dc35dc82
Cezar Spatariu-Neagu <cezar.spatariu@gmail.com>
2016-07-20 14:21:12 +0200

    Change the timeout for the connection to polox

--- status_lisk/controllers.py
+++ status_lisk/controllers.py
@@ -9,7 +9,7 @@ def make_request(url, api_path, params):
     url_full = url + api_path + params
     app.logger.debug(url_full)
     try:
-        response = requests.get(url_full, params=params, timeout=0.5,)
+        response = requests.get(url_full, params=params, timeout=1,)
         if response.status_code == 200:
             return response.json()
     except requests.exceptions.Timeout:
@@ -22,7 +22,7 @@ def make_poloniex_req(params):
     url_full = 'https://poloniex.com/public'
     params = {'command' : params } 
     try:
-        response = requests.get(url_full, params=params, timeout=0.5,)
+        response = requests.get(url_full, params=params, timeout=1,)
         if response.status_code == 200:
             return response.json()
     except requests.exceptions.Timeout:
